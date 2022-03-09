e6eb2cf20b9b0146f1dde8ff07f3763771891c89
Viet Nguyen <viet@ofload.com>
2022-03-09 16:01:05 +0700

    add account sandbox-aft-01 and sandbox-aft-02

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,50 +26,79 @@
 #   account_customizations_name = "sandbox"
 # }
 
-module "sandbox_aft_01" {
+module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
     AccountEmail              = "awslab.08@gmail.com" 
     AccountName               = "sandbox-aft-01"
-    ManagedOrganizationalUnit = "Workloads"
+    ManagedOrganizationalUnit = "Learn AFT"
     SSOUserEmail              = "awslab.02@gmail.com"
-    SSOUserFirstName          = "NOT"
-    SSOUserLastName           = "USE"
+    SSOUserFirstName          = "Sandbox"
+    SSOUserLastName           = "AFT"
   }
 
   account_tags = {
-    "Project"     = "test"
-    "Environment" = "Production"
-    "AFT"         = true
+    "Learn Tutorial" = "AFT"
   }
 
   change_management_parameters = {
-    change_requested_by = "vietwow"
-    change_reason       = "Test AFT 1"
+    change_requested_by = "VietWOW"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
 }
 
-module "sandbox_aft_02" { 
-  source = "./modules/aft-account-request"
 
-  control_tower_parameters = {
-    AccountEmail              = "awslab.10@gmail.com"
-    AccountName               = "sandbox-aft-02"
-    ManagedOrganizationalUnit = "Workloads"
-    SSOUserEmail              = "awslab.02@gmail.com"
-    SSOUserFirstName          = "NOT"
-    SSOUserLastName           = "USE"
-  }
+# module "sandbox_aft_01" {
+#   source = "./modules/aft-account-request"
 
-  account_tags = {
-    "Project"     = "test"
-    "Environment" = "Production"
-    "AFT"         = true
-  }
+#   control_tower_parameters = {
+#     AccountEmail              = "awslab.08@gmail.com" 
+#     AccountName               = "sandbox-aft-01"
+#     ManagedOrganizationalUnit = "Workloads"
+#     SSOUserEmail              = "awslab.02@gmail.com"
+#     SSOUserFirstName          = "NOT"
+#     SSOUserLastName           = "USE"
+#   }
 
-  change_management_parameters = {
-    change_requested_by = "vietwow"
-    change_reason       = "Test AFT 2"
-  }
-}
+#   account_tags = {
+#     "Project"     = "test"
+#     "Environment" = "Production"
+#     "AFT"         = true
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "vietwow"
+#     change_reason       = "Test AFT 1"
+#   }
+# }
+
+# module "sandbox_aft_02" { 
+#   source = "./modules/aft-account-request"
+
+#   control_tower_parameters = {
+#     AccountEmail              = "awslab.10@gmail.com"
+#     AccountName               = "sandbox-aft-02"
+#     ManagedOrganizationalUnit = "Workloads"
+#     SSOUserEmail              = "awslab.02@gmail.com"
+#     SSOUserFirstName          = "NOT"
+#     SSOUserLastName           = "USE"
+#   }
+
+#   account_tags = {
+#     "Project"     = "test"
+#     "Environment" = "Production"
+#     "AFT"         = true
+#   }
+
+#   change_management_parameters = {
+#     change_requested_by = "vietwow"
+#     change_reason       = "Test AFT 2"
+#   }
+# }
